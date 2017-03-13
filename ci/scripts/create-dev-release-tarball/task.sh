629fa68f0438c2f881b9d5737a9803ee64add79d
Valeria Perticara <vperticara@it.ibm.com>
2017-03-14 00:28:19 +0100

    CI: use old bosh cli for release creation
    [#140770777]

--- ci/scripts/create-dev-release-tarball/task.sh
+++ ci/scripts/create-dev-release-tarball/task.sh
@@ -12,9 +12,14 @@ function main(){
   local root="${1}"
 
   export BOSH_ENVIRONMENT="https://${BOSH_DIRECTOR}:25555"
+  /opt/rubies/ruby-2.2.4/bin/bosh target ${BOSH_ENVIRONMENT}
+  /opt/rubies/ruby-2.2.4/bin/bosh login ${BOSH_CLIENT} ${BOSH_CLIENT_SECRET}
 
   pushd ${root}/dev-release
-  bosh create-release --force --tarball="${root}/dev-release-tarball/${REL_NAME}_${REL_VERSION}.tgz" --version "${REL_VERSION}" --name "${REL_NAME}"
+  /opt/rubies/ruby-2.2.4/bin/bosh -t ${BOSH_DIRECTOR} create release --force --with-tarball --version "${REL_VERSION}" --name "${REL_NAME}"
+  cp dev_releases/${REL_NAME}/${REL_NAME}-${REL_VERSION}.tgz ${root}/dev-release-tarball
+
+  #bosh create-release --force --tarball="${root}/dev-release-tarball/${REL_NAME}_${REL_VERSION}.tgz" --version "${REL_VERSION}" --name "${REL_NAME}"
   popd
 }
 
