493305687328bf872d76d23ea43b4e6747d58884
Valeria Perticara <vperticara@it.ibm.com>
2017-03-16 14:19:32 +0100

    CI: fix cf-upgrade pipeline

--- ci/scripts/test-with-diego/task.sh
+++ ci/scripts/test-with-diego/task.sh
@@ -2,7 +2,7 @@
 
 function main() {
   export BOSH_ENVIRONMENT="https://${BOSH_DIRECTOR}:25555"
-  bosh -n -d $DEPLOYMENT_NAME run errand acceptance_tests
+  bosh -n -d $DEPLOYMENT_NAME run-errand acceptance_tests
 }
 
 main
