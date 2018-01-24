a0f4ba886f702f5e45f002491ba3d03d1472d062
Valeria Perticara <vperticara@it.ibm.com>
2018-01-24 14:28:16 +0100

    ci: fix tarball creation

--- ci/scripts/create-dev-release-tarball/task.sh
+++ ci/scripts/create-dev-release-tarball/task.sh
@@ -15,12 +15,9 @@ function main(){
   local root="${1}"
   preflight_check
   export BOSH_ENVIRONMENT="https://${BOSH_DIRECTOR}:25555"
-  echo "${BOSH_CA_CERT}" > ${root}/ca_cert
-  /opt/rubies/ruby-2.2.4/bin/bosh --ca-cert=${root}/ca_cert -u ${BOSH_CLIENT} -p ${BOSH_CLIENT_SECRET} target ${BOSH_ENVIRONMENT}
 
   pushd ${root}/dev-release
-  /opt/rubies/ruby-2.2.4/bin/bosh -t ${BOSH_DIRECTOR} create release --force --with-tarball --version "${REL_VERSION}" --name "${REL_NAME}"
-  cp dev_releases/${REL_NAME}/${REL_NAME}-${REL_VERSION}.tgz ${root}/dev-release-tarball
+  bosh create-release --force --tarball=${root}/dev-release-tarball/${REL_NAME}-${REL_VERSION}.tgz --version "${REL_VERSION}" --name "${REL_NAME}"
   popd
 }
 
