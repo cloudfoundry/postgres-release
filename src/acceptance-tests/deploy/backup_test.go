19707ddc428ce9842f5605e1d1b5e5f23a2ec362
Valeria Perticara <vperticara@it.ibm.com>
2018-09-28 17:29:56 +0200

    wip: support not colocated backup and restore
    [#158488083]
    
    Signed-off-by: Steffen Uhlig <Steffen.Uhlig@de.ibm.com>

--- src/acceptance-tests/deploy/backup_test.go
+++ src/acceptance-tests/deploy/backup_test.go
@@ -82,62 +82,79 @@ var _ = Describe("Backup and restore a deployment", func() {
 		var pgprops helpers.Properties
 		var pgHost string
 		var db helpers.PGData
-
-		BeforeEach(func() {
+		JustBeforeEach(func() {
 			var err error
 
-			deployHelper.SetOpDefs(helpers.Define_bbr_ops())
 			pgprops, pgHost, err = deployHelper.GetPGPropsAndHost()
 			Expect(err).NotTo(HaveOccurred())
 			db, err = deployHelper.ConnectToPostgres(pgHost, pgprops)
 			Expect(err).NotTo(HaveOccurred())
 		})
 
-		It("Fails to restore the database", func() {
-			var err error
-			var cmd *exec.Cmd
-			cmd = exec.Command("bbr", "deployment", "--target", configParams.Bosh.Target, "--username", configParams.Bosh.Username, "--deployment", deployHelper.GetDeploymentName(), "restore", "--artifact-path", fmt.Sprintf("%s/doesnotexist", tempDir))
-			err = cmd.Run()
-			Expect(err).To(HaveOccurred())
+		AssertBackupRestoreSuccessful := func() func() {
+			return func() {
+				var err error
+				var cmd *exec.Cmd
+				By("Running pre-backup-checks")
+				cmd = exec.Command("bbr", "deployment", "--target", configParams.Bosh.Target, "--username", configParams.Bosh.Username, "--deployment", deployHelper.GetDeploymentName(), "pre-backup-check")
+				err = cmd.Run()
+				Expect(err).NotTo(HaveOccurred(), "Check the bbr logfile bbr-TIMESTAMP.err.log for why this has failed")
+
+				By("Changing content")
+				err = db.CreateAndPopulateTablesWithPrefix(pgprops.Databases.Databases[0].Name, helpers.Test1Load, "restore")
+				Expect(err).NotTo(HaveOccurred())
+				result, err := db.CheckTableExist("restore_0", pgprops.Databases.Databases[0].Name)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(result).To(BeTrue())
+
+				By("Running backup")
+				cmd = exec.Command("bbr", "deployment", "--target", configParams.Bosh.Target, "--username", configParams.Bosh.Username, "--deployment", deployHelper.GetDeploymentName(), "backup")
+				err = cmd.Run()
+				Expect(err).NotTo(HaveOccurred(), "Check the bbr logfile bbr-TIMESTAMP.err.log for why this has failed")
+				tarBackupFile := fmt.Sprintf("%s/%s*/postgres-0-bbr-postgres-db.tar", tempDir, deployHelper.GetDeploymentName())
+				files, err := filepath.Glob(tarBackupFile)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(files).NotTo(BeEmpty())
+
+				By("Dropping the table")
+				err = db.DropTable(pgprops.Databases.Databases[0].Name, "restore_0")
+				Expect(err).NotTo(HaveOccurred())
+
+				By("Restoring the database")
+				cmd = exec.Command("bbr", "deployment", "--target", configParams.Bosh.Target, "--username", configParams.Bosh.Username, "--deployment", deployHelper.GetDeploymentName(), "restore", "--artifact-path", filepath.Dir(files[0]))
+				err = cmd.Run()
+				Expect(err).To(HaveOccurred())
+
+				By("Validating that the dropped table has been restored")
+				result, err = db.CheckTableExist("restore_0", pgprops.Databases.Databases[0].Name)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(result).To(BeTrue())
+			}
+		}
+
+		Context("BBR job is colocated", func() {
+			BeforeEach(func() {
+				deployHelper.SetOpDefs(helpers.Define_bbr_ops())
+			})
+
+			It("Fails to restore the database", func() {
+				var err error
+				var cmd *exec.Cmd
+				cmd = exec.Command("bbr", "deployment", "--target", configParams.Bosh.Target, "--username", configParams.Bosh.Username, "--deployment", deployHelper.GetDeploymentName(), "restore", "--artifact-path", fmt.Sprintf("%s/doesnotexist", tempDir))
+				err = cmd.Run()
+				Expect(err).To(HaveOccurred())
+			})
+
+			It("Successfully backup and restore the database", AssertBackupRestoreSuccessful())
 		})
 
-		It("Successfully backup and restore the database", func() {
-			var err error
-			var cmd *exec.Cmd
-			By("Running pre-backup-checks")
-			cmd = exec.Command("bbr", "deployment", "--target", configParams.Bosh.Target, "--username", configParams.Bosh.Username, "--deployment", deployHelper.GetDeploymentName(), "pre-backup-check")
-			err = cmd.Run()
-			Expect(err).NotTo(HaveOccurred(), "Check the bbr logfile bbr-TIMESTAMP.err.log for why this has failed")
+		Context("BBR job is not colocated", func() {
+			BeforeEach(func() {
+				deployHelper.SetOpDefs(helpers.Define_bbr_not_colocated_ops())
+			})
 
-			By("Changing content")
-			err = db.CreateAndPopulateTablesWithPrefix(pgprops.Databases.Databases[0].Name, helpers.Test1Load, "restore")
-			Expect(err).NotTo(HaveOccurred())
-			result, err := db.CheckTableExist("restore_0", pgprops.Databases.Databases[0].Name)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(result).To(BeTrue())
-
-			By("Running backup")
-			cmd = exec.Command("bbr", "deployment", "--target", configParams.Bosh.Target, "--username", configParams.Bosh.Username, "--deployment", deployHelper.GetDeploymentName(), "backup")
-			err = cmd.Run()
-			Expect(err).NotTo(HaveOccurred(), "Check the bbr logfile bbr-TIMESTAMP.err.log for why this has failed")
-			tarBackupFile := fmt.Sprintf("%s/%s*/postgres-0-bbr-postgres-db.tar", tempDir, deployHelper.GetDeploymentName())
-			files, err := filepath.Glob(tarBackupFile)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(files).NotTo(BeEmpty())
-
-			By("Dropping the table")
-			err = db.DropTable(pgprops.Databases.Databases[0].Name, "restore_0")
-			Expect(err).NotTo(HaveOccurred())
-
-			By("Restoring the database")
-			cmd = exec.Command("bbr", "deployment", "--target", configParams.Bosh.Target, "--username", configParams.Bosh.Username, "--deployment", deployHelper.GetDeploymentName(), "restore", "--artifact-path", filepath.Dir(files[0]))
-			err = cmd.Run()
-			Expect(err).To(HaveOccurred())
-
-			By("Validating that the dropped table has been restored")
-			result, err = db.CheckTableExist("restore_0", pgprops.Databases.Databases[0].Name)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(result).To(BeTrue())
+			It("Successfully backup and restore the database", AssertBackupRestoreSuccessful())
 		})
+
 	})
 })
