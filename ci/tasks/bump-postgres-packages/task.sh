d21e98540893de4b608866e438c60018c1806ac5
I539231 <pascal.zimmermann01@sap.com>
2024-05-21 12:11:00 +0000

    fix: Rename the package and adapt the CI jobs

--- ci/tasks/bump-postgres-packages/task.sh
+++ ci/tasks/bump-postgres-packages/task.sh
@@ -19,6 +19,9 @@ pushd postgres-release
   fi
 
   if ${NEED_COMMIT}; then
+    latest_yq_version=$(curl -s -L https://api.github.com/repos/mikefarah/yq/releases/latest | grep "tag_name" | sed s/\"tag_name\":\//g | sed s/\"//g | sed s/\,//g | sed s/v//g | xargs)
+    curl -s -L https://github.com/mikefarah/yq/releases/download/v${latest_yq_version}/yq_linux_amd64 -o /tmp/yq && chmod +x /tmp/yq
+
     echo "-----> $(date): Update the PostgreSQL version inside the used_postgresql_versions.yml file"
     current_minor_version=$(cat config/blobs.yml  | grep "postgresql-${MAJOR_VERSION}" | cut -f1 -d: | sed "s/postgres\/postgresql-//g" | sed "s/.tar.gz//g")
     CURRENT_MINOR_VERSION=$current_minor_version /tmp/yq -i '.postgresql.major_version[env(MAJOR_VERSION)].minor_version = strenv(CURRENT_MINOR_VERSION)' jobs/postgres/config/used_postgresql_versions.yml
--- ci/tasks/bump-yq-packages/task.sh
+++ ci/tasks/bump-yq-packages/task.sh
@@ -7,15 +7,15 @@ echo "${PRIVATE_YML}" > postgres-release/config/private.yml
 
 pushd postgres-release
   CURRENT_BLOBS=$(bosh blobs)
-  CURRENT_VERSION=$(curl -s -L https://api.github.com/repos/mikefarah/yq/releases/latest | grep "tag_name" | sed s/\"tag_name\":\//g | sed s/\"//g | sed s/\,//g | sed s/v//g | xargs)
-  mv ../yq-src/yq_darwin_amd64 ../yq-src/yq-${CURRENT_VERSION}
-  BLOB_PATH=$(ls ../yq-src/yq-${CURRENT_VERSION})
+  CURRENT_VERSION=$(cat ../yq-src/version)
+  mv ../yq-src/yq_linux_amd64 ../yq-src/postgres-yq-${CURRENT_VERSION}
+  BLOB_PATH=$(ls ../yq-src/postgres-yq-${CURRENT_VERSION})
   FILENAME=$( basename ${BLOB_PATH} )
-  OLD_BLOB_PATH=$(cat config/blobs.yml  | grep "yq-${MAJOR_VERSION}" | cut -f1 -d:)
+  OLD_BLOB_PATH=$(cat config/blobs.yml  | grep "postgres-yq-${MAJOR_VERSION}" | cut -f1 -d:)
   if ! echo "${CURRENT_BLOBS}" | grep "${FILENAME}" ; then
     NEED_COMMIT=true
     echo "adding ${FILENAME}"
-    bosh add-blob --sha2 "${BLOB_PATH}" "yq/${FILENAME}"
+    bosh add-blob --sha2 "${BLOB_PATH}" "yq/postgres-${FILENAME}"
     bosh remove-blob ${OLD_BLOB_PATH}
     bosh upload-blobs
   fi
