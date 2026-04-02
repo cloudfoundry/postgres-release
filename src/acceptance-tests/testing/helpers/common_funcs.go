e8e36e3e1e4c2dfc2aa4d79a7970da0f6b9e456a
Pascal Zimmermann <pascal.zimmermann01@sap.com>
2026-04-02 15:09:54 +0200

    feat: Use the ISO format for the date

--- src/acceptance-tests/testing/helpers/common_funcs.go
+++ src/acceptance-tests/testing/helpers/common_funcs.go
@@ -72,7 +72,7 @@ func (d *DeployHelper) InitializeVariables() {
 
 	d.variables["superuser_name"] = "superuser"
 	d.variables["superuser_password"] = "superpsw"
-	d.variables["superuser_valid_until"] = time.Now().AddDate(0, 1, 0).Format("Jan 2 2006")
+	d.variables["superuser_valid_until"] = time.Now().AddDate(0, 1, 0).Format("2006-01-02")
 	d.variables["testuser_name"] = "sshuser"
 	d.variables["postgres_dns"] = fmt.Sprintf("q-s0.postgres.%s.%s.bosh", d.networkName, d.name)
 }
